9af81348a584e435b48812059c5ecf8bc416e409
Carlos Domingo <163530177+ProfLidenbrock@users.noreply.github.com>
2024-03-16 13:33:51 +0100

    Create comando_a_fichero.c

---
+++ Pipes/Ejemplos dup y dup2/comando_a_fichero.c
@@ -0,0 +1,28 @@
+/*
+Ejemplo donde se hace uso de la llamada dup2() y donde se envia la ejecucion de EXEC a un fichero de texto
+*/
+
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+    if (fork() == 0) {      // Proceso HIJO
+
+        int fd = open("test.txt", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+
+        dup2(fd, 1);   // make stdout go to file
+        dup2(fd, 2);   // make stderr go to file - you may choose to not do this
+                       // or perhaps send stderr to another file
+
+        close(fd);     // fd no longer needed - the dup'ed handles are sufficient
+
+        execlp("ls", "ls", NULL);
+
+    } else {                // Proceso PADRE
+
+        while (wait(NULL) > 0) {} // wait for each child process
+    }
+
+    return 0;
+}
